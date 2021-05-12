9cadf5eb9f8c45dc27be9bde1c2481c82df7ccb4
kimminkyu <kmk50009@naver.com>
2021-05-12 18:26:49 +0900

    Tinder 이메일 로그인 구현

---
+++ app/src/main/java/fastcampus/aop/part3/chapter05/LoginAcitivity.kt
@@ -0,0 +1,93 @@
+package fastcampus.aop.part3.chapter05
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginAcitivity: AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        auth = Firebase.auth
+
+        initLoginButton()
+        initSignUpButton()
+        initEmailAndPasswordEditText()
+    }
+
+    private fun initLoginButton() {
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val email = getInputEmail()
+            val password = getInputPassword()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful){
+                        finish()
+                    } else {
+                      Toast.makeText(this,"로그인에 실패했습니다. 이메일 또는 비밀번호를 확인해주세요." , Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+
+    private fun initSignUpButton() {
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+        signUpButton.setOnClickListener {
+            val email = getInputEmail()
+            val password = getInputPassword()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "회원가입에 성공했습니다. 로그인 버튼을 눌러 로그인해 주세요.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(this, "이미 가입한 이메일이거나, 회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+        }
+    }
+
+    private fun getInputEmail(): String {
+        return findViewById<EditText>(R.id.emailEditText).text.toString()
+    }
+
+    private fun getInputPassword(): String {
+        return findViewById<EditText>(R.id.passwordEditText).text.toString()
+    }
+
+    private fun initEmailAndPasswordEditText() {
+
+        val emailEditText = findViewById<EditText>(R.id.emailEditText)
+        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        emailEditText.addTextChangedListener {
+            val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled = enable
+            signUpButton.isEnabled = enable
+        }
+
+        passwordEditText.addTextChangedListener {
+            val enable = emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()
+            loginButton.isEnabled = enable
+            signUpButton.isEnabled = enable
+        }
+    }
+
+
+
+}
--- app/src/main/java/fastcampus/aop/part3/chapter05/MainActivity.kt
+++ app/src/main/java/fastcampus/aop/part3/chapter05/MainActivity.kt
@@ -1,12 +1,26 @@
 package fastcampus.aop.part3.chapter05
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(auth.currentUser == null) {
+            startActivity(Intent(this, LoginAcitivity::class.java))
+        }
     }
 }
